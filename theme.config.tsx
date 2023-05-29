76904eefb62cac7f3d67677c048b7e839002a81e
Ruan Silva <ruanrafaelleite@gmail.com>
2023-05-28 23:32:54 -0300

    removed default options

--- theme.config.tsx
+++ theme.config.tsx
@@ -3,7 +3,21 @@ export default {
     project: {
       link: 'https://github.com/Ruanrls/compilador'
     },
+    search: {
+      component: null
+    },
+    editLink: {
+      component: null
+    },
+    feedback: {
+      content: null
+    },
     footer: {
       component: null
+    },
+    useNextSeoProps() {
+      return {
+        titleTemplate: 'Ruan – %s',
+      }
     }
 }
